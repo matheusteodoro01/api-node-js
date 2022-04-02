ec8fbab7202f0be9dee5228afb568d3d49faa169
Matheus Teodoro <matheusteodoro01@hotmail.com>
2022-04-02 14:34:55 -0300

    feat: find, update and delete finalides on CarController

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -1,12 +1,81 @@
 import { Request, Response } from "express";
+import Car from "../database/models/CarModel";
+
 
 
 const CarController = {
 
-    async index(request: Request, response: Response): Promise<Response> {
+    async index(req: Request, res: Response): Promise<Response> {
 
-        return response.json({ message: "Wello Word" })
+        let cars = await Car.find()
 
+
+        return res.json(cars)
+    },
+
+
+    async findById(req: Request, res: Response): Promise<Response> {
+
+        const { id } = req.params;
+
+        let cars = await Car.findById(id)
+
+
+        return res.json(cars)
+    },
+
+    async create(req: Request, res: Response): Promise<Response> {
+
+        await Car.create(req.body)
+            .then(data => {
+                return res.json(data)
+            })
+            .catch(error => {
+                return res.status(400).json(error.original)
+            })
+
+        return res.status(500)
+    },
+
+    async update(req: Request, res: Response): Promise<Response> {
+
+
+        const { id } = req.params;
+        const { marca, modelo, versao, ano, quilometragem, tipoCambio, precoVenda } = req.body
+
+        await Car.findByIdAndUpdate(id, {
+            marca: marca,
+            modelo: modelo,
+            versao: versao,
+            ano: ano,
+            quilometragem: quilometragem,
+            tipoCambio: tipoCambio,
+            precoVenda: precoVenda
+        })
+            .then(data => {
+                return res.json(data)
+            })
+            .catch(error => {
+                return res.status(400).json(error.original)
+            })
+
+        return res.status(500)
+
+    },
+
+    async delete(req: Request, res: Response): Promise<Response> {
+
+        const { id } = req.params;
+
+        await Car.findByIdAndDelete(id)
+
+            .then(data => {
+                return res.json({ message: `${id} exluido com sucesso!` })
+            })
+            .catch(error => {
+                return res.status(400).json(error.original)
+            })
+        return res.status(500)
 
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -9,7 +9,9 @@ router.use((req, res, next) => {
 })
 
 router.get('/cars', CarController.index)
-
-
+router.get('/car/:id', CarController.findById)
+router.post('/car', CarController.create)
+router.put('/car/:id', CarController.update)
+router.delete('/car/:id', CarController.delete)
 
 export default router
