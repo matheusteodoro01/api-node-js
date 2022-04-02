7d031230a25362e6e0d388fd82f66724e69a5c90
Matheus Teodoro <matheusteodoro01@hotmail.com>
2022-04-02 14:33:32 -0300

    feat: connection with mongo db

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from 'express'
 import routes from './routes'
+import mongoose from "mongoose";
 
 const app = express()
 
+app.use(express.json())
 app.use(routes)
 
+mongoose.connect('mongodb+srv://matheus:Ma16194830@apinode.dzkxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 
-const PORT = 3333
+    .then((data) => {
+        console.log('MongoDB Connection Succeeded', data.version);
+    })
 
-app.listen(PORT)
+    .catch((err) => {
+        console.log('Error in DB connection:', err.message);
+    })
+
+
+app.listen(3333,()=> console.log('Server running on port http://localhost:3333'))
